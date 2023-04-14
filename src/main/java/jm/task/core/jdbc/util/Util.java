75ec377f1f9459c7af067bbd350a15a4b7a81b43
desno31 <desno31@gmail.com>
2023-04-14 16:43:51 +0300

    Сделал настройки НЕ через XML файл

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,18 +2,28 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/bdkatatest?autoReconnect=true&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String DIALECT = "org.hibernate.dialect.MySQLDialect";
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bdkatatest"
-                    , "root", "root");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException("no connection", e);
         }
@@ -21,8 +31,23 @@ public class Util {
     }
 
     public static SessionFactory getSessionFactory () {
-        SessionFactory factory = new Configuration().configure()
-                .addAnnotatedClass(User.class).buildSessionFactory();
+        Configuration config = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, DRIVER);
+        settings.put(Environment.URL, URL);
+        settings.put(Environment.USER, USERNAME);
+        settings.put(Environment.PASS, PASSWORD);
+        settings.put(Environment.DIALECT, DIALECT);
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        config.setProperties(settings);
+        config.addAnnotatedClass(User.class);
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(config.getProperties()).build();
+        SessionFactory factory = config.buildSessionFactory(serviceRegistry);
+
+        //настройки через ХМЛ файл
+        /*        SessionFactory factory = new Configuration().configure()
+                .addAnnotatedClass(User.class).buildSessionFactory();*/
         return factory;
     }
 }
